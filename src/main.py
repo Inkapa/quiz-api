77c7363e9bc13f2e00cf6d10501dcdd0693454b9
Liam Cornu <71927979+Inkapable@users.noreply.github.com>
2022-01-01 02:04:53 +0100

    added port from env

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 import uvicorn
 import src.endpoints
 import src.config
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 if __name__ == "__main__":
-    # to play with API run the script and visit http://127.0.0.1:8000/docs
-    uvicorn.run("src.main:src.config.app")  # you can use reload=True in dev mode
+    # to play with API run the script and visit http://0.0.0.0:$PORT/docs
+    uvicorn.run("src.main:src.config.app", host="0.0.0.0", port=os.getenv("PORT"))  # you can use reload=True in dev mode
