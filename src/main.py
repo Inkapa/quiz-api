2c5e67c1f4a86e1acd81c26352050cbdf6ce0370
Liam Cornu <71927979+Inkapable@users.noreply.github.com>
2022-01-01 02:06:26 +0100

    Cast to int

--- src/main.py
+++ src/main.py
@@ -7,4 +7,4 @@ import os
 load_dotenv()
 if __name__ == "__main__":
     # to play with API run the script and visit http://0.0.0.0:$PORT/docs
-    uvicorn.run("src.main:src.config.app", host="0.0.0.0", port=os.getenv("PORT"))  # you can use reload=True in dev mode
+    uvicorn.run("src.main:src.config.app", host="0.0.0.0", port=int(os.getenv("PORT")))  # you can use reload=True in dev mode
