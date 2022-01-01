40cbba3c6950b95d29bcd9579576b78c3fb80b58
Liam Cornu <71927979+Inkapable@users.noreply.github.com>
2022-01-01 01:53:06 +0100

    comment unused code

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
-import uvicorn
+# import uvicorn
 from fastapi import FastAPI
 
-if __name__ == "__main__":
-    app = FastAPI()
-    # to play with API run the script and visit http://127.0.0.1:8000/docs
-    uvicorn.run("main:src.config.app", host="127.0.0.1", port=8000)  # you can use reload=True in dev mode
+app = FastAPI()
+
+# if __name__ == "__main__":
+# to play with API run the script and visit http://127.0.0.1:8000/docs
+# uvicorn.run("main:src.config.app", host="127.0.0.1", port=8000)  # you can use reload=True in dev mode
